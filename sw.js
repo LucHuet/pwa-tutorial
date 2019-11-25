deea484ee1d553c8124136af7ffd54846fa72f07
Luc Huet <luc.huet.external@buyin.pro>
2019-11-25 23:32:21 +0100

    Dynamic cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -25,6 +26,15 @@ self.addEventListener('install', evt => {
 //activate event
 self.addEventListener('activate', evt => {
     //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    )
 });
 
 //fetch event
@@ -32,7 +42,12 @@ self.addEventListener('fetch', evt => {
     //console.log('service worker has been fetching', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes =>{
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
-    )
+    );
 });
