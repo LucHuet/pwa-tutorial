b08f4582ca671029849839b186b94a7b3078ba18
Luc Huet <luc.huet.external@buyin.pro>
2019-11-27 18:38:00 +0100

    Firestore add and remove

--- js/db.js
+++ js/db.js
@@ -1,3 +1,16 @@
+//offline data
+
+db.enablePersistence()
+    .catch(err => {
+        if( err.code == 'failed-precondition'){
+            //probably multi tabs open
+            console.log('persistence failed');
+        }else if(err.code == 'unimplemented') {
+            //lack of browser support
+           console.log('persistence is not available');
+        }
+    });
+
 //real time listener
 db.collection('recipes').onSnapshot(snapshot => {
     //console.log(snapshot.docChanges());
@@ -5,9 +18,38 @@ db.collection('recipes').onSnapshot(snapshot => {
         console.log(change.type, change.doc.id, change.doc.data());
         if(change.type === 'added'){
             // add the document data to the web page
+            renderRecipe(change.doc.data(), change.doc.id);
         }
         if(change.type === 'removed'){
             // remove the document data from the web page
+            removeRecipe(change.doc.id);
         }
     });
+});
+
+//add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const recipe = {
+      title: form.title.value,
+      ingredients: form.ingredients.value,
+    };
+
+    db.collection('recipes').add(recipe)
+        .catch(err => console.log(err));
+
+    form.title.value = '';
+    form.ingredients.value = '';
+});
+
+//delete a recipe
+const recipeContainer = document.querySelector('.recipes');
+recipeContainer.addEventListener('click', evt => {
+   //console.log(evt);
+    if(evt.target.tagName === 'I'){
+        const id = evt.target.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete();
+    }
 });
